8e1cc604415834f656b2f93885eef086601d9847
BarneyCampbell <43809465+BarneyCampbell@users.noreply.github.com>
2023-04-27 11:19:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-function findNum() {
-    var x = parseInt(document.getElementById("demo").textContent);
-    
-    if(x % 2 == 0) {
-        x = x + 3;
 
-    } else {
-        x = x + 7;
-    }
-
-    document.getElementById("demo").innerHTML = x;
-}
